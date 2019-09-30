175c8d7346943a7972413f8ee17808a45ea144d7
Matthew Clark <matthew.clark.dev@gmail.com>
2019-09-29 23:09:45 -0500

    Add AI interface and random implementation.

--- snake_ai/ai.py
+++ snake_ai/ai.py
@@ -1,4 +1,5 @@
 import random
+from abc import ABC, abstractmethod
 
 import numpy as np
 import pandas as pd
@@ -7,8 +8,59 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 
 
-# TODO(matthew-c21) - Hook up this model so it may be trained.
-class Agent:
+class Agent(ABC):
+    """Interface for all snake-playing deep learning agents."""
+    @abstractmethod
+    def make_choice(self, game_state):
+        """Determines a choice based on internal state and the given game state.
+
+        :returns an integer 0-3 corresponding to a directional input UP, DOWN, LEFT, or RIGHT."""
+        pass
+
+    @abstractmethod
+    def set_reward(self, reward):
+        """Reward or punish the agent for the resulting decision.
+
+        :param reward an integer representing the reward for an action. Negative values are considered punishments.
+            Larger magnitudes correspond to more strongly weighted rewards."""
+        pass
+
+    @abstractmethod
+    def dump_weights(self):
+        """Output a list of weights that affect the decision making process."""
+        pass
+
+    @abstractmethod
+    def remember(self, state, action, reward, next_state, done):
+        """Commit the given data to memory."""
+        pass
+
+    @abstractmethod
+    def train_short_memory(self, state, action, reward, next_state, done):
+        """Train the model according to its short term memory."""
+        pass
+
+
+class RandomAgent(Agent):
+    """Implements virtually no methods, instead opting to choose moves at random for testing purposes."""
+    def make_choice(self, game_state):
+        return np.random.randint(4)
+
+    def set_reward(self, reward):
+        pass
+
+    def dump_weights(self):
+        pass
+
+    def remember(self, state, action, reward, next_state, done):
+        pass
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        pass
+
+
+# TODO(matthew-c21) - Finish implementing this model.
+class DefaultAgent:
     def __init__(self):
         self.reward = 0
         self.gamma = 0
@@ -34,7 +86,6 @@ class Agent:
         return state
 
     def set_reward(self, board, snake):
-        # TODO(matthew-c21) - Ensure that the GameState class has this method or an equivalent.
         if not board.is_playable():
             self.reward -= 100
         elif snake.head.has_eaten:
